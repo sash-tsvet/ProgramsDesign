d735bac43a14b8e17bd61c8826495fde33398871
SomeFire <SomeFireOne@gmail.com>
2016-09-28 20:24:47 +0300

    Small renames

--- src/ShapeUtils.java
+++ src/ShapeUtils.java
@@ -2,7 +2,7 @@
  * Created by user17 on 28.09.2016.
  */
 public class ShapeUtils {
-	public static Shape getMaxShape(Shape cube, Shape sphere) {
-		return (cube.volume() > sphere.volume())? cube: sphere;
+	public static Shape getMaxShape(Shape shape1, Shape shape2) {
+		return (shape1.volume() > shape2.volume())? shape1: shape2;
 	}
 }
